32d69a707fb44b9adf64f816c1be2f90bbec5517
Owen Jones <jonesor@gmail.com>
2024-09-14 17:34:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## READ ME
+
+### Things to do each year:
+
+- Style check: Run `styler::style_dir(filetype = "Rmd")`
+- Spell check: use `SpellCheckScript.R`
+- Rebuild GitHub site. (`Build Book` button in RStudio)
+
---
+++ preamble.R
@@ -0,0 +1,14 @@
+#packages for the main content
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(magrittr)
+library(patchwork)
+
+#Packages for the web pages
+library(xfun)
+library(htmltools)
+library(mime)
+
+library(officer)
+library(flextable)
